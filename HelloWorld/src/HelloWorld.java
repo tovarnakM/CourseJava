eb2599c5167b574e24a442b9902cfdf0a489f488
tovarnakM <36849758+tovarnakM@users.noreply.github.com>
2018-09-12 18:00:11 +0200

    Initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("First java program");
+
+        int myFirstNumer = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumer * 2;
+
+        int myTotal = myFirstNumer + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+    }
+}
