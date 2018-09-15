18de0104648d91da47f1acc03069d60e18bf60d0
tovarnakM <36849758+tovarnakM@users.noreply.github.com>
2018-09-15 19:07:59 +0200

    fg

---
+++ KeywordsAndExpressions/src/com/tovarnak/Main.java
@@ -0,0 +1,33 @@
+package com.tovarnak;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //mile is equal to 1.609344 kilometers
+        double km = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50){
+            System.out.println("This is an expression");
+        }
+
+        //                  *****EXERCISE*****
+        //
+        //
+        //  1. In the following code that I will type below,
+        //     write down what part of this code are expression.
+        //
+        //
+        int score = 100;
+        if (score > 99){
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+        // a, score = 100
+        // b, score > 99
+        // c, "You got the high score!"
+        // d, score = 0
+
+
+
+    }
+}
---
+++ StatementsWhiteSpaceAndIndenting/src/com/tovarnak/Main.java
@@ -0,0 +1,22 @@
+package com.tovarnak;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //whole line below is a statement
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                "another " +
+                "still more.");
+        int anotherVariable = 5;
+        myVariable--;
+        System.out.println("This is another one");
+
+        //CODE-REFORMAT CODE for indentation
+    }
+}
