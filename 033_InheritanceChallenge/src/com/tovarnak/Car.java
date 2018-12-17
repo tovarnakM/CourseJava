8f70274ad2f5d5d118a2950da04eaf9d3fe44296
tovarnakM <36849758+tovarnakM@users.noreply.github.com>
2018-12-18 00:48:10 +0100

    sda

---
+++ 033_InheritanceChallenge/src/com/tovarnak/Car.java
@@ -0,0 +1,8 @@
+package com.tovarnak;
+
+public class Car extends Vehicle {
+
+    public Car(int velocity, String direction, int acceleration, String name) {
+        super(velocity, direction, acceleration, name);
+    }
+}
---
+++ 033_InheritanceChallenge/src/com/tovarnak/Mercedes.java
@@ -0,0 +1,8 @@
+package com.tovarnak;
+
+public class Mercedes extends Car {
+
+    public Mercedes(int velocity, String direction, int acceleration) {
+        super(velocity, direction, acceleration, "Mercedes");
+    }
+}
