651e1a9d20b08495785e4a1bcb71cf4235abda40
tovarnakM <36849758+tovarnakM@users.noreply.github.com>
2018-12-24 03:18:27 +0100

    dasads

---
+++ 042_referencesVsValueTypes/src/com/tovarnak/Main.java
@@ -0,0 +1,55 @@
+package com.tovarnak;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue = 10;
+        int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        int[] myIntArray = new int[5];
+        int[] anotherIntArray = myIntArray;
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray = " + Arrays.toString(anotherIntArray));
+
+        anotherIntArray[0] = 1;
+
+        System.out.println("after change myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherArray = " + Arrays.toString(anotherIntArray));
+
+        anotherIntArray = new int[] {4,5,6,7,8,9};
+        modifyArray(myIntArray);
+
+        System.out.println("after modify myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after modify anotherArray = " + Arrays.toString(anotherIntArray));
+    }
+    private static void modifyArray(int[] array){
+        array[0] = 2;
+        array = new int[] {1,2,3,4,5};
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
