001986e38a77898f06626f6dc510e674f7084d5b
tovarnakM <36849758+tovarnakM@users.noreply.github.com>
2020-02-16 02:55:12 +0100

    asdas

--- 006_Strings/eventapp/src/components/CardList.js
+++ 006_Strings/eventapp/src/components/CardList.js
@@ -19,36 +19,43 @@ const CardList = (props) => {
     }
 
     const renderEventsAdmin = () => {
-        
         return props.events.map((event,i) => {
             return <div key={i} className="cardList__item">
-                        
-                        {
-                            i === 0 ?
-                                <div className="addEventToExisting">
-                                    <h1>+</h1>
-                                    <p>Add New Event</p>
-                                </div> 
-                            :
-                            <div className="eventInfo">
-                                <div style={{fontSize: '4rem'}}>{event.title}</div>
-                                <div style={{fontSize: '2.5rem', marginTop: '1.8rem'}}>{event.date}</div>
-                                <div style={{fontSize: '1.5rem', marginTop: '1rem'}}>Location: {event.location}</div>
-                                <div className="info_container">
-                                    <div>Date: {event.date}</div>
-                                    <div>Price: {event.price}$</div>
-                                </div>
-                                <button style={{marginTop: '1rem'}} className="header-button">Edit event</button>
+                        <div className="eventInfo">
+                            <div style={{fontSize: '4rem'}}>{event.title}</div>
+                            <div style={{fontSize: '2.5rem', marginTop: '1.8rem'}}>{event.date}</div>
+                            <div style={{fontSize: '1.5rem', marginTop: '1rem'}}>Location: {event.location}</div>
+                            <div className="info_container">
+                                <div>Date: {event.date}</div>
+                                <div>Price: {event.price}$</div>
                             </div>
-                        }
+                            <button style={{marginTop: '1rem'}} className="header-button">Edit event</button>
+                        </div>
                     </div>
         })
     }
 
 
     return (
-        <div className="cardList">
-            { props.isAdmin ? renderEventsAdmin() : renderEventsUser()}
+        <div>            
+            {   
+                props.isAdmin ?
+                    <div className="cardList">
+                        <div className="cardList__item">
+                            <div className="addEventToExisting">
+                                <div>
+                                    <h1>+</h1>
+                                </div>
+                                <p>Add New Event</p>
+                            </div> 
+                        </div>
+                        {renderEventsAdmin()} 
+                    </div>
+                : 
+                <div className="cardList">
+                    {renderEventsUser()}
+                </div>
+            }
         </div>
     );
 };
